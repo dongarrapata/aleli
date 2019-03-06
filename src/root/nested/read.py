6502e92249d5da84ffc916424e358d7aea5e8f5f
dongarrapata <almanzorrr@gmail.com>
2019-03-06 17:54:53 -0600

    Se completa el ejemplo de búsqueda

--- src/root/nested/read.py
+++ src/root/nested/read.py
@@ -6,6 +6,12 @@ Created on Mar 6, 2019
 import imaplib
 import email
 
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else:
+        return msg.get_payload(None,True)
+
 m = imaplib.IMAP4_SSL("imap.gmail.com", 993)
 m.login("","")
 m.select('"[Gmail]/All Mail"')
@@ -17,6 +23,9 @@ if result == 'OK':
         if result == 'OK':
             email_message = email.message_from_bytes(data[0][1])    # raw email text including headers
             print('From:' + email_message['From'])
+            print(get_body(email_message))
+
+m.search(None,'(FROM "msarmientonavarro@gmail.com")')
 
 m.close()
 m.logout()
