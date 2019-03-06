944bbd1820ec9e3055d7c2556e3cb269ba6a6d79
dongarrapata <almanzorrr@gmail.com>
2019-03-06 15:58:15 -0600

    Agrega leeCorreo

---
+++ src/root/nested/leeCorreo.py
@@ -0,0 +1,20 @@
+import imaplib, email
+
+user = ''
+password = ''
+imap_url = 'imap.gmail.com'
+
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else:
+        return msg.get_payload(None,True)
+
+con = imaplib.IMAP4_SSL(imap_url)
+con.login(user, password)
+con.select('INBOX')
+
+result, data = con.fetch(b'3', '(RFC822)')
+raw = email.message_from_bytes(data[0][1])
+
+print(raw)
--- src/root/nested/lilalila.py
+++ src/root/nested/lilalila.py
@@ -3,4 +3,5 @@ Created on Mar 6, 2019
 
 @author: almanzor
 '''
-print('Hola ', abs(-3), 'adiós.')
+
+
